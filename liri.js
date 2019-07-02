efa85ddc2fad0b3976d60dde1e1066b482bb0dc8
Anu KC <anukc2000@gmail.com>
2019-07-01 23:38:09 -0500

    working on bands

--- liri.js
+++ liri.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 
 var axios = require("axios");
 
-var keys= require("./keys.js");
+var keys = require("./keys.js");
 
-var Spotify = require('node-spotify-api')
+var Spotify = require('node-spotify-api');
+
+var bandsintown = require('bandsintown');
 
 var spotify = new Spotify(keys.spotify);
 
@@ -30,6 +32,9 @@ console.log("First test: " + userInput);
 // break;
 
 // }
+
+//function to call the movie
+/*
 
 function movie() {
 
@@ -74,9 +79,85 @@ function movie() {
 
         
 
-}
+};
 
 console.log( "This is the info: " + movie());
+
+*/
+
+
+
+function band() {
+
+    var artistname = userInput.join("%20")
+
+    console.log(artistname)
+    var queryUrlBands = "https://rest.bandsintown.com/artists/" + artistname + "/events?app_id=codingbootcamp";
+
+
+    console.log("this is second test: " + queryUrlBands);
+
+    axios.get(queryUrlBands).then(
+
+        function (response) {
+
+        //   var date = response.data[0].datetime.slice(0,10)
+        //   var year = date.slice(0,4);
+        //   var month = date.slice(5,7)
+        //   var day = date.slice(8,11)
+        //   var properDate = month + "/" + day+ "/" + year
+        //   console.log(properDate)
+       
+        //         console.log("Date: " + properDate);
+        //         console.log("Name of the venue" + response.data[0].venue.name);
+        //         console.log("Venue location: " + response.data[0].venue.city)
+
+        for (i=0; i<2; i++) {
+            var date = response.data[i].datetime.slice(0,10)
+        var year = date.slice(0,4);
+        var month = date.slice(5,7)
+        var day = date.slice(8,11)
+        var properDate = month + "/" + day+ "/" + year
+        console.log(properDate)
+     
+              console.log("Date: " + properDate + "\n");
+              console.log("Name of the venue" + response.data[i].venue.name + "\n");
+              console.log("Venue location: " + response.data[i].venue.city)
+              console.log("-------------Dataseparator---------------")
+
+        }
+        
+            
+            
+        }
+    )
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+console.log( "This is the info: " + band());
+
+
+
+
+
 
 
 // var apiCall = process.argv[2];
@@ -85,7 +166,7 @@ console.log( "This is the info: " + movie());
 
 /*
 userInput = switch(apicall) {
-    case "movie-this": 
+    case "movie-this":
     getMovies);
     break;
     case "spotify-this":
@@ -100,42 +181,3 @@ userInput = switch(apicall) {
 
 
 
-//OMDAPI- movie info.
-
-/*
-
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("Title of the movie: " + response.data.Title);
-    console.log("Year the movie came out: " + response.data.Year);
-    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes Rating of the movie is: " + response.data.Ratings.Value);
-    console.log("Country where the movie was produced is: " + response.data.Country);
-    console.log("Language of the movie is: " + response.data.Language);
-    console.log("Plot of the movie: " + response.data.Plot);
-    console.log("Actors in the movie: " + response.data.Actors)
-
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-
-
-*/
